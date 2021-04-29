c16ada8cf12d588712a0ce1c3bf4250266e7f9e4
Louis Riedel <s3004072@ba-sachsen.de>
2021-04-29 21:44:07 +0200

    Transfer Test 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for (int i=0; i < 3; i++) {
+            System.out.println("Send this to the laptop!");
+        }
     }
 }
