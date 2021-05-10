8b2d774cc3055a6134bdaa71bcc69b65a90d455b
Louis Riedel <s3004072@ba-sachsen.de>
2021-05-10 21:23:01 +0200

    6. GridLayout

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,19 @@ public class Main {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
-        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10,10));
+        frame.setLayout(new GridLayout(3,3,10,10));
 
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(100, 250));
-        panel.setBackground(Color.lightGray);
-        panel.setLayout(new FlowLayout());
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+        frame.add(new JButton("10"));
 
-        panel.add(new JButton("1"));
-        panel.add(new JButton("2"));
-        panel.add(new JButton("3"));
-        panel.add(new JButton("4"));
-        panel.add(new JButton("5"));
-        panel.add(new JButton("6"));
-        panel.add(new JButton("7"));
-        panel.add(new JButton("8"));
-        panel.add(new JButton("9"));
-
-        frame.add(panel);
         frame.setVisible(true);
     }
 
