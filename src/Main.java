39328da0e2e40893a2b1ca145ce62422c4ccd798
Louis Riedel <s3004072@ba-sachsen.de>
2021-05-10 21:23:48 +0200

    7. GridLayout

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.setLayout(new GridLayout(3,3,10,10));
-
+        //
         frame.add(new JButton("1"));
         frame.add(new JButton("2"));
         frame.add(new JButton("3"));
