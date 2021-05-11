fbd079e8fb5adb7564a94bfe0812fdeda0a733ff
Louis Riedel <s3004072@ba-sachsen.de>
2021-05-11 18:21:24 +0200

    10. JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,19 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        LaunchPage launchPage = new LaunchPage();
+        //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.PLAIN_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.INFORMATION_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.QUESTION_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.WARNING_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.ERROR_MESSAGE);
 
+        //int answer = JOptionPane.showConfirmDialog(null, "bro, do you even code", "this is my title", JOptionPane.YES_NO_CANCEL_OPTION);
+        //String name = JOptionPane.showInputDialog("What is your name?");
+        //System.out.println("Hello Mr. " + name);
+
+        String[] responses = {"No, you're awesome!", "thank you!", "*blush*"};
+
+        JOptionPane.showOptionDialog(null,"You are awesome", "title", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, responses, 0);
     }
 }
 
