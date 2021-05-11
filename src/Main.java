1e58eba955ba67e885ee3b74a5578baf5437972d
Louis Riedel <s3004072@ba-sachsen.de>
2021-05-11 11:25:22 +0200

    9. Open a new Window

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,8 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        JLabel label1 = new JLabel();
-        label1.setOpaque(true);
-        label1.setBackground(Color.red);
-        label1.setBounds(50,50,200,200);
+        LaunchPage launchPage = new LaunchPage();
 
-        JLabel label2 = new JLabel();
-        label2.setOpaque(true);
-        label2.setBackground(Color.green);
-        label2.setBounds(100,100,200,200);
-
-        JLabel label3 = new JLabel();
-        label3.setOpaque(true);
-        label3.setBackground(Color.blue);
-        label3.setBounds(150,150,200,200);
-
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.add(label1, Integer.valueOf(0));
-        layeredPane.add(label2, Integer.valueOf(2));
-        layeredPane.add(label3, Integer.valueOf(1));
-        layeredPane.setBounds(0,0,500,500);
-
-        JFrame frame = new JFrame();
-        frame.add(layeredPane);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setLayout(null);
-
-        frame.setVisible(true);
     }
-
 }
 
